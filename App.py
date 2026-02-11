5fdba01e83dbaed35f705cf97af5229da2a489e1
santana.gabrielmenezes <santana.gabrielmenezes+github@gmail.com>
2026-02-11 13:47:21 -0300

    inserindo arquivos para início do projeto

---
+++ App.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+st.write("""
+         Neste projeto, aplicamos o algoritmo de clusterização K-means para identificar e prever agrupamentos de interesses de usuários, com o objetivo de direcionar campanhas de marketing de forma mais eficaz.
+         Através dessa análise, conseguimos segmentar o público em bolhas de interesse, permitindo a criação de campanhas personalizadas e mais assertivas, com base nos padrões de comportamento e preferências de cada grupo.
+         """)
+
+
+
+
+
+
+
+
+    # st.write("""
+    #             ### Descrição dos Grupos:
+    #             - **Grupo 0** é focado em um público jovem com forte interesse em moda, música e aparência.
+    #             - **Grupo 1** está muito associado a esportes, especialmente futebol americano, basquete e atividades culturais como banda e rock.
+    #             - **Grupo 2** é mais equilibrado, com interesses em música, dança, e moda.
+    #          """)
